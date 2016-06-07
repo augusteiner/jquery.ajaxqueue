9a79a146bc842fff1bf3739269c61717b8c6d98f
José Nascimento <joseaugustodearaujonascimento@gmail.com>
2016-06-07 20:11:31 -0300

    better code formatting

--- index.js
+++ index.js
@@ -1,33 +1,45 @@
-jQuery(function(){
+jQuery(function () {
 
-    return function($){
+    return function ($) {
+
         var handlr = $.when();
 
         $.ajaxSetup({
             dataType: 'json',
             method: 'GET',
             timeout: 30000,
-            success: function(){ },
-            error: function(xhr, e) { }
+            success: function () { },
+            error: function (xhr, e) { }
         });
 
-        $('#send').submit(function(e) {
+        $('#send').submit(function (e) {
+
             var $this = $(this);
 
             e.preventDefault();
 
             // XXX Trying better ajax handling
-            handlr = handlr.done(function(action){
-                return function(){
-                    $.ajax({
+            handlr = handlr.done(function (action) {
+
+                return function () {
+
+                    console.log("sending ajax request" + new Date());
+
+                    var dfd = $.ajax({
                         url: action
-                    }).done(function(r) {
-                        console.log(r);
+                    }).done(function (r) {
+
+                        console.log("received response");
                     });
+
+                    while (!dfd.isResolved() ||
+                        !dfd.isRejected()) {
+
+                        //
+                    }
                 };
             }(this.action));
-
-            console.log("request sent to " + this.action);
         });
     };
-}(jQuery));
+}(jQuery.noConflict()));
+
